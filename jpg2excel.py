73810244a2098895ba33c7bf033dd216fd8f4e4f
mmodin <mathias.concha@gmail.com>
2019-08-15 00:48:07 +0200

    Fixed stupid stuff and add readme

--- jpg2excel.py
+++ jpg2excel.py
@@ -1,27 +1,30 @@
 import xlsxwriter
 import sys
+import os
 from PIL import Image
 
 def validate_file_size(im):
 	pass
 
 
-def jpg2excel(file_name):
-	im = Image.open(file_name)
+def jpg2excel(fp):
+	file_name, path = os.path.splitext(os.path.basename(fp))
+	im = Image.open(fp)
 	w = im.width
 	h = im.height
 	print(f"Reading {file_name}[{w}x{h}] from disk...")
 	hex_colors = ['#%02x%02x%02x' % x for x in im.getdata()]
-	excel_name = file_name.split(".")[0] + ".xlsx"
+	
+	excel_name = file_name + ".xlsx"
 	print(f"Writing data to excel sheet: {excel_name}...")
 	workbook = xlsxwriter.Workbook(excel_name)
 	worksheet = workbook.add_worksheet()
 	index = 0
-	for row in range(im.width):
-		for col in range(im.height):
+	for row in range(h):
+		for col in range(w):
 			cell_format = workbook.add_format()
 			cell_format.set_bg_color(hex_colors[index])
-			worksheet.write(row, col, "", cell_format)
+			worksheet.write(row, col, ., cell_format)
 			index += 1
 	print("Done! Closing file.")
 	workbook.close()
---
+++ readme.md
@@ -0,0 +1,11 @@
+# jpg2excel.py
+
+### Example usage
+
+```python
+git clone https://github.com/mmodin/jpg2excel.git
+cd jpg2excel
+python jpg2excel.py kim.jpg
+```
+
+Enjoy :)
